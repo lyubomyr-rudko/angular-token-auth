b82432bfd3d449f742a17adfbc92c35c81608ece
lyubomyr.rudko <rudkolg@gmail.com>
2015-07-24 16:48:56 +0300

    Added passport module for login - FIXED

--- api/api.js
+++ api/api.js
@@ -3,10 +3,17 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var User = require('./models/user.js');
 var jwt = require('./services/jwt.js');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
 app.use(bodyParser.json());
+app.use(passport.initialize());
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
 
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
@@ -16,6 +23,37 @@ app.use(function (req, res, next) {
 	next();
 });
 
+var strategy = new LocalStrategy({
+	usernameField: 'email'
+}, function (email, password, done) {
+	var searchUser = {
+		email: email
+	};
+
+	User.findOne(searchUser, function (err, user) {
+		if (err) {
+			return done(err);
+		}
+
+		if (!user) {
+			return done(null, false, {message: 'Wrong email/password'}); 
+		}
+
+		user.comparePasswords(password, function (err, isMatch) {
+			if (err) {
+				return done(err);
+			}
+
+			if (!isMatch) {
+				return done(null, false, {message: 'Wrong email/password'}); 
+			}
+
+			return done(null, user);
+		});
+	});
+});
+
+passport.use(strategy);
 
 app.post('/api/register', function (req, res) {
 	var user = req.body;
@@ -34,34 +72,20 @@ app.post('/api/register', function (req, res) {
 	});
 });
 
-app.post('/api/login', function (req, res) {
-	req.user = req.body;
-
-	var searchUser = {
-		email: req.user.email
-	};
-
-	User.findOne(searchUser, function (err, user) {
+app.post('/api/login', function (req, res, next) {
+	passport.authenticate('local', function (err, user) {
 		if (err) {
-			throw err;
+			next(err);
 		}
 
-		if (!user) {
-			return res.status(401).send({message: 'Wrong email/password'});
-		}
-
-		user.comparePasswords(req.user.password, function (err, isMatch) {
+		req.login(user, function (err) {
 			if (err) {
-				throw err;
+				next(err);
 			}
 
-			if (isMatch) {
-				createAndSendToken(user, res);
-			} else {
-				res.status(401).send({message: 'Wrong email/password'});
-			}
+			createAndSendToken(user, res);
 		});
-	});
+	})(req, res, next);
 });
 
 function createAndSendToken (user, res) {
